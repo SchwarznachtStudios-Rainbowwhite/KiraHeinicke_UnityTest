040c88aebc86cad15aca15b48a51d6be63b51f57
SchwarznachtStudios-Rainbowwhite <Rainbowwhite@gmx.de>
2023-12-20 10:41:45 +0100

    Number Filter

--- KiraHeinicke_UnityTest/Assets/Scripts/goodCode/NumberFilter.cs
+++ KiraHeinicke_UnityTest/Assets/Scripts/goodCode/NumberFilter.cs
@@ -1,21 +1,72 @@
 using UnityEngine;
+using System.Linq;
 using System.Collections.Generic;
 
 public class NumberFilter : MonoBehaviour
 {
-    /*
-   Aufgabenstellung: Refaktorisierung
 
-   - Refaktorisieren Sie das Skript, um die Zahlenreihe effizienter nach geraden und ungeraden Zahlen zu filtern.
-   - Nutzen Sie Arrays und foreach-Schleifen für die Refaktorisierung.
-   - Ergänzen Sie Codeblöcke mit Kommentaren zur Beschreibung ihrer Funktion.
-   - Fügen Sie zwei Methoden hinzu, die jeweils alle geraden bzw. ungeraden Zahlen lesbar in die Konsole schreiben.
-   - Nach jeder Refaktorisierung erfolgt ein Push auf Git mit deskriptiven Namen.
-   - Nach Abschluss aller Refaktorisierungen laden Sie oli90martin@web.de als Collaborator zu Ihrer Git-Repository ein.
-   */
+// VARIABLES VARIABLES
 
-    public int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    public int[] evenNumbers;
-    public int[] oddNumbers;
+    public int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    public int[] EvenNumbers;
+    public int[] OddNumbers;
 
+    private int _counterEven = 0;
+    private int _counterOdd = 0;
+
+    // START
+
+    public void Start()
+    {
+        SortList();
+
+        WriteEvenNumbers();
+        WriteOddNumbers();
+    }
+
+    // FUNCTIONS FUNCTIONS
+
+    public void SortList()
+    {
+        foreach (int number in Numbers)
+        {
+            // Wenn Zahl Gerade, zu even numbers
+            // _counterEven nach jeder hinzugefügten Zahl hochgezählt, um jeweils einen nächsten Platz im Array zu besetzen
+            if (number % 2 == 0)
+            {          
+                EvenNumbers[_counterEven] = number;
+                _counterEven++;
+            }
+            // Wenn Zahl Ungerade, zu odd numbers
+            // _counterOdd nach jeder hinzugefügten Zahl hochgezählt, um jeweils einen nächsten Platz im Array zu besetzen
+            else if (number % 2 == 1)
+            {
+                OddNumbers[_counterOdd] = number;
+                _counterOdd++;
+            }
+
+           
+        }
+
+    }
+
+    public void WriteEvenNumbers()
+    {
+        // Zählt durch das Array durch und gibt einzeln jede Nummer aus
+        for (int i = 0; i < EvenNumbers.Length; i++)
+        {
+            Debug.Log("EvenNumber: " + EvenNumbers[i]);
+        }
+
+    }
+
+    public void WriteOddNumbers()
+    {
+        // Zählt durch das Array durch und gibt einzeln jede Nummer aus
+        for (int i = 0; i < OddNumbers.Length; i++)
+        {
+            Debug.Log("OddNumber: " + OddNumbers[i]);
+        }
+
+    }
 }
