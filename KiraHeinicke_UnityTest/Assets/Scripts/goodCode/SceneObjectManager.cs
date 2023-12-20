61bae92fb25e110cdad1e0be1d05cd6a53ac81dd
SchwarznachtStudios-Rainbowwhite <Rainbowwhite@gmx.de>
2023-12-20 11:52:38 +0100

    SceneObjectManager
    Random Position der Items nicht funktional: Out of Array Bounds

--- KiraHeinicke_UnityTest/Assets/Scripts/goodCode/SceneObjectManager.cs
+++ KiraHeinicke_UnityTest/Assets/Scripts/goodCode/SceneObjectManager.cs
@@ -1,21 +1,82 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class SceneObjectManager : MonoBehaviour
 {
-    public GameObject[] enemies;
-    public GameObject[] items;
-    public string[] enemyTypes = { "fire", "water", "ice", "stone" };
-    public Vector3 itemPositionRange = new Vector3(10f, 10f, 10f);
 
-    /*
-    Aufgabenstellung: Refaktorisierung
+// VARIABLES VARIABLES
+    public GameObject[] Enemies;
+    public GameObject[] Items;
 
-    - Refaktorisieren Sie das Skript, um alle Objekte mit dem Tag "Enemy" einem Array von Feinden und alle Objekte mit dem Tag "Item" einem Array von Gegenständen effizient hinzuzufügen.
-    - Weisen Sie jedem "Enemy"-Objekt einen zufälligen Typ aus dem enemyTypes-Array zu.
-    - Weisen Sie jedem "Item"-Objekt eine zufällige Position innerhalb des angegebenen itemPositionRange-Bereichs zu.
-    - Erstellen Sie 2 Button Methoden, Button 1 soll jeden Enemy mit dem dazugehörigen Typ in die Console schreiben. Botton 2 soll jedes Item mit der jeweilig dazugehörigen Position in die Console schreiben.
-    - Ergänzen Sie Codeblöcke mit Kommentaren zur Beschreibung ihrer Funktion.
-    - Nach jeder Refaktorisierung erfolgt ein Push auf Git mit deskriptiven Namen.
-    - Nach Abschluss aller Refaktorisierungen laden Sie oli90martin@web.de als Collaborator zu Ihrer Git-Repository ein.
-    */
+
+    public string[] EnemyTypes = { "fire", "water", "ice", "stone" };
+    
+
+    Dictionary<GameObject, string> DictionaryEnemies = new Dictionary<GameObject, string>();
+    Dictionary<GameObject, Vector3> DictionaryItems = new Dictionary<GameObject, Vector3>();
+
+// START START
+
+    public void Start()
+    {
+
+        SortByTag();
+        EnemyType();
+        //ItemPosition();
+
+        WriteEnemyAndType();
+        //WriteItemAndPosition();
+    }
+
+// FUNCTIONS FUNCTIONS
+
+    public void SortByTag()
+    {
+        // Sucht alle GameObjects mit entsprechendem Tag in der Szene und fügt sie dem jweiligen Array hinzu
+        Enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        Items = GameObject.FindGameObjectsWithTag("Item");
+
+    }
+
+    public void EnemyType()
+    {
+        // Jedes Enemy Object wird dem Dictionary zugeordnet, sowie jedem ein zufälliger Typ aus enemyTypes
+        for (int i = 0; i < Enemies.Length; i++)
+        {
+            DictionaryEnemies.Add(Enemies[i], EnemyTypes[Random.Range(0,EnemyTypes.Length)]);
+
+        }
+    }
+
+    /*public void ItemPosition()
+    {
+        for (int i = 0; i < Items.Length; i++)
+        {
+            DictionaryItems.Add(Enemies[i], new Vector3(Random.Range(0, 10f), Random.Range(0, 10f), Random.Range(0, 10f)));
+            //RandomRange Position gibt noch out of bounds error
+        }
+
+    }*/
+
+    public void WriteEnemyAndType()
+    {
+        // Zählt durch das Dictionary durch und gibt Enemy mit zugehörigem Typ aus
+        foreach (KeyValuePair<GameObject, string> items in DictionaryEnemies)
+        {
+            print("Enemy: " + items.Key + " of Type: " + items.Value);
+
+        }
+
+    }
+
+    public void WriteItemAndPosition()
+    {
+        // Zählt durch das Dictionary durch und gibt Item mit zugehöriger Position aus
+        foreach (KeyValuePair<GameObject, Vector3> items in DictionaryItems)
+        {
+            print("Item: " + items.Key + " at Position: " + items.Value);
+
+        }
+
+    }
 }
